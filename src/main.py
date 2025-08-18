b6a16815b15fa13de0720581c16f07490f287336
anoodh-ai <anoodhvpmiv@gmail.com>
2025-08-18 23:13:28 +0530

    Added main.py file

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import joblib
+
+model = joblib.load(r"C:\Users\anoodhivi\2025-2026 projects\truth-checker-ai\models\fake_news_model.pkl")
+vectorizer = joblib.load(r"C:\Users\anoodhivi\2025-2026 projects\truth-checker-ai\models\vectorizer.pkl")
+
+
+def check_news(news_text):
+    transformed_text = vectorizer.transform([news_text])
+    prediction = model.predict(transformed_text)[0]
+    return "❌ Fake News" if prediction == 1 else "✅ Real News"
+
+# Example
+if __name__ == "__main__":
+    sample = input("Enter news text: ")
+    result = check_news(sample)
+    print(result)
